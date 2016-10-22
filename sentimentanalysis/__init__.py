79b02931c3114e60678d8227bbee968ef127ec67
David Morton <davidmortonmail@gmail.com>
2016-10-22 23:48:32 +0100

    Updated sentiment analysis service

---
+++ sentimentanalysis/__init__.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify, json
+from flask import request
+import pickle
+import ast
+import sys
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Sentiment Analysis service is online'
+
+@app.route('/api/analyse', methods=['POST', 'GET'])
+def sentiment_analysis():
+    # Load pickled objects instead of retraining them every time
+    count_vectorizer = pickle.load(open("count_vectorizer.pkl", "rb"))
+    classifier = pickle.load(open("sentiment_classifier.pkl", "rb"))
+
+    comment = []
+    # Test Phase
+    if request.method == 'POST':
+        value = request.json['review']
+        #comment = ['this movie was so bad']
+    if request.method == 'GET':
+        value = request.args['review']
+        #comment = ['this movie was so bad']
+
+    comment.append(value)
+    labelResult = classifier.predict(count_vectorizer.transform(comment))
+
+    if labelResult[0] == '1':
+        reviewSentiment = 'Positive'
+    else:
+        reviewSentiment = 'Negative'
+
+    return jsonify(comment, reviewSentiment)
